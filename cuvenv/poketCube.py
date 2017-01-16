c83fb4cacca04b74368061567891a735fdc4ba51
sinsky <wesky93@gmail.com>
2017-01-16 17:30:29 +0900

    get 메소드 테스트 코드 추가

--- cuvenv/poketCube.py
+++ cuvenv/poketCube.py
@@ -147,24 +147,40 @@ if __name__ == "__main__" :
 
     # face 객체 테스트
     sample1 = [ [ 1, 6, 4 ], [ 2, 4, 1 ], [ 4, 5, 1 ] ]
-    sample1np = np.array( sample1,dtype=np.uint8 )
+    sample1np = np.array( sample1, dtype=np.uint8 )
     sample2 = [ [ 1, 1, 1 ], [ 1, 1, 1 ], [ 1, 1, 1 ] ]
-    n = 3
-    newface = face( 3 )
 
+    # 면의 길이
+    n = 3
+    
     # n*n의 면 생성
+    newface = face( n )
+
     print( "{n}*{n} 면 생성\n면 상태:\n{matrix}".format( n=n, matrix=newface.matrix ) )
 
     print( "\n면에 리스트 타입의 값을 부여" )
     newface.set( sample1 )
     print( "값 부여\n면 상태:\n{matrix}\n완셩여부: {done} 면 점수: {point}".format( matrix=newface.matrix, done=newface.done,
-                                                                         point=newface.point ) )
+                                                                       point=newface.point ) )
     print( "\nndarray 타입의 값을 부여" )
     newface.set( sample1np )
     print( "값 부여\n면 상태:\n{matrix}\n완셩여부: {done} 면 점수: {point}".format( matrix=newface.matrix, done=newface.done,
-                                                                         point=newface.point ) )
+                                                                       point=newface.point ) )
     # 면이 완성 됬을 경우
     newface.set( sample2 )
-    print( "\n값 부여\n면 상태:\n{matrix}\n완셩여부: {done} 면 점수: {point}".format( matrix=newface.matrix, done=newface.done,
+    print( "\n면 상태:\n{matrix}\n완셩여부: {done} 면 점수: {point}".format( matrix=newface.matrix, done=newface.done,
                                                                          point=newface.point ) )
+    # get 메소드 테스트
+    newface.set( sample1np )
+    index1 = "r0"
+    index2 = "c1"
+    index3 = "r2"
+
+    print( "\n면 상태:\n{matrix}\n완셩여부: {done} 면 점수: {point}".format( matrix=newface.matrix, done=newface.done,
+                                                                   point=newface.point ) )
+
+    print( "{index} : {result}".format( index=index1, result=newface.get( index1 ) ) )
+    print( "{index} : {result}".format( index=index2, result=newface.get( index2 ) ) )
+    print( "{index} : {result}".format( index=index3, result=newface.get( index3 ) ) )
+
     # change 메소드 테스트

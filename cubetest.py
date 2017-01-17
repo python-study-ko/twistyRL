ba6354375907000583972d7b96eca306403f8a5a
sinsky <wesky93@gmail.com>
2017-01-17 16:05:44 +0900

    테스트 코드 분리

--- cubetest.py
+++ cubetest.py
@@ -10,17 +10,24 @@ def testpoket( ) :
     def reset( cube ) :
         for i in range( 1, 7 ) :
             cube.cube[ i ].set( sampleCube[ i ] )
-        print( "포켓큐브 리셋\n", cube.cube )
+        # print( "포켓큐브 리셋\n", cube.cube )
         return cube
 
     sampleCube = {
         1 : [ [ 11, 12 ], [ 13, 14 ] ], 2 : [ [ 21, 22 ], [ 23, 24 ] ], 3 : [ [ 31, 32 ], [ 33, 34 ] ],
         4 : [ [ 41, 42 ], [ 43, 44 ] ], 5 : [ [ 51, 52 ], [ 53, 54 ] ], 6 : [ [ 61, 62 ], [ 63, 64 ] ]
     }
+
+    # 회전 테스트
     poket = poketCube( )
-    poket = reset( poket )
-    poket.action( 'F' )
-    print( "F 실행\n",poket.cube )
+    # turnset = ('F', 'F`', 'R', 'R`', 'U', 'U`', 'B', 'B`', 'L', 'L`', 'D', 'D`')
+    turnset = ('F')
+
+    print( "회전 테스트" )
+    for act in turnset:
+        poket = reset(poket)
+        poket.action(act)
+        print("{act} 실행:\n{cube}".format(act=act,cube=poket.cube))
 
 
 if __name__ == "__main__" :

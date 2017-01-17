cee727636af0fc6ca42e25ed29c679b83f1246e8
sinsky <wesky93@gmail.com>
2017-01-17 12:09:56 +0900

    큐브 클래스 추가

--- cuvenv/poketCube.py
+++ cuvenv/poketCube.py
@@ -176,6 +176,95 @@ class Cube :
             # 면의 값을 초기화 시킨다 ex) 3번 면의 값은 모두 3으로 초기화
             self.cube[ i ].reset( i )
 
+    def check(self):
+        """
+        면 상태 체크 메소드, 큐브가 변경되는 시점마다 호출하여 완셩여부와 점수를 확인한다.
+        :return:
+        """
+
+class poketCube( Cube ) :
+    """
+    포켓큐브(2*2*2) 게임
+    """
+
+    def __init__( self ) :
+        self.make( 2 )
+
+
+
+    def copy( self, faces ) :
+        """
+        딕셔너리에 적혀 있는대로 면을 복사해 준다
+        :param faces: 키는 면의 인덱스, 값은 복사할 값이 있는 인덱스
+        :return:
+        """
+        dic = faces
+        assert type(dic) == type(dict())
+
+        for i in dic :
+            # 면에서 원하는 인덱스 값을 복사하여 알려준다
+            dic[i] = self.cube[i].get(dic[i])
+        return dic
+
+    # todo: np.flip(mat,0) 와 np.fliplr(mat)을 이용하여
+
+    def action( self, act ) :
+        """
+        큐브를 회전시키는 메소드
+        :param act: 명령어셋
+        :return:
+        """
+        turn = act
+        # 가능한 명령어 셋
+        turnset = ('F', 'F`', 'R', 'R`', 'U', 'U`', 'B', 'B`', 'L', 'L`', 'D', 'D`')
+        # 명령어셋 확인
+        assert turn in turnset
+
+        if turn == 'F' :
+            pass
+        elif turn == 'F`' :
+            pass
+        elif turn == 'R' :
+            pass
+        elif turn == 'R`' :
+            pass
+        elif turn == 'U' :
+            pass
+        elif turn == 'U`' :
+            pass
+        elif turn == 'B' :
+            pass
+        elif turn == 'B`' :
+            pass
+        elif turn == 'L' :
+            pass
+        elif turn == 'L`' :
+            pass
+        elif turn == 'D' :
+            pass
+        elif turn == 'D`' :
+            pass
+
+
+def testpoket( ) :
+    """
+    포켓큐브 회전 개발용 테스트 함수
+    :return:
+    """
+
+    def reset( cube ) :
+        for i in range( 1, 7 ) :
+            cube.cube[ i ].set( sampleCube[ i ] )
+        print( "포켓큐브 리셋\n", cube.cube )
+        return cube
+
+    sampleCube = {
+        1 : [ [ 11, 12 ], [ 13, 14 ] ], 2 : [ [ 21, 22 ], [ 23, 24 ] ], 3 : [ [ 31, 32 ], [ 33, 34 ] ],
+        4 : [ [ 41, 42 ], [ 43, 44 ] ], 5 : [ [ 51, 52 ], [ 53, 54 ] ], 6 : [ [ 61, 62 ], [ 63, 64 ] ]
+        }
+    poket = poketCube( )
+    poket = reset( poket )
+
 
 if __name__ == "__main__" :
     ## 테스트 코드
@@ -240,5 +329,12 @@ if __name__ == "__main__" :
     print( "변경후 면상태\n{matrix}".format( matrix=newface.matrix ) )
 
     # reset 메소드 테스트
-    newface.reset(3)
-    print("큐브 초기화\n{matrix}".format(matrix=newface.matrix))
+    newface.reset( 3 )
+    print( "면 초기화\n{matrix}".format( matrix=newface.matrix ) )
+
+    ## 큐브 클래스 테스트
+    # 포켓큐브 생성
+    poket = poketCube( )
+    print( "큐브 생성\n", poket.cube )
+
+    testpoket( )

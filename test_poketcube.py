dece0bd5dd094f8306e3d909135f316e6fe09d77
sinsky <wesky93@gmail.com>
2017-01-17 18:32:31 +0900

    테스트 코드 분리
    핵심 클래스 테스트와 개별 큐브 테스트를 분리함

---
+++ test_poketcube.py
@@ -0,0 +1,39 @@
+from cuvenv import *
+
+
+def testpoket( ) :
+    """
+    포켓큐브 회전 개발용 테스트 함수
+    :return:
+    """
+
+    sampleCube = {
+        1 : [ [ 11, 12 ], [ 13, 14 ] ], 2 : [ [ 21, 22 ], [ 23, 24 ] ], 3 : [ [ 31, 32 ], [ 33, 34 ] ],
+        4 : [ [ 41, 42 ], [ 43, 44 ] ], 5 : [ [ 51, 52 ], [ 53, 54 ] ], 6 : [ [ 61, 62 ], [ 63, 64 ] ]
+    }
+
+    def reset( cube ) :
+        for i in range( 1, 7 ) :
+            cube.cube[ i ].set( sampleCube[ i ] )
+        # print( "포켓큐브 리셋\n", cube.cube )
+        return cube
+
+    # 회전 테스트
+    poket = poketCube( )
+    # turnset = ('F', 'F`', 'R', 'R`', 'U', 'U`', 'B', 'B`', 'L', 'L`', 'D', 'D`')
+    turnset = ('F')
+
+    print( "회전 테스트" )
+    for act in turnset:
+        poket = reset(poket)
+        poket.action(act)
+        print("{act} 실행:\n{cube}".format(act=act,cube=poket.cube))
+
+
+if __name__ == "__main__" :
+    ## 테스트 코드
+    # 포켓큐브 생성
+    poket = poketCube( )
+    print( "큐브 생성\n", poket.cube )
+
+    testpoket( )

2ac8c97ffe937fe68786d09d0774439b483a0c94
sinsky <wesky93@gmail.com>
2017-01-31 21:45:38 +0900

    0.1.20 업데이트
    check()메소드 제거 및 @property 적용 반영

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dependency_links = [
 
 setup(
         name='twistyRL',
-        version='0.1.19',
+        version='0.1.20',
         url='https://github.com/python-study-ko/twistyRL',
         license='MIT License',
         description='twisty cube game for ML',
